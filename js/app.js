d086d2853f101beefaed4c52f6affbee3acfe936
ethan375 <ethanebel1@gmail.com>
2017-12-17 16:08:03 -0600

    Commit 2 - It's all about Me done

--- js/app.js
+++ js/app.js
@@ -2,7 +2,30 @@
 let clicker = {
   clickCount: 0,
   // everytime this function is run it will increment clickCount
-  click () => {
-    clickCount++
+  click: function(){
+    clicker.clickCount++
+  }
+}
+
+
+//Its all about me
+//declaring an object myInformation that has 4 propeties
+//the statement method combines all of the properties into a single output
+//ageincrementor method increases the age by 1 every time it is called
+//indentity crisis method changes name to rumplestillskin
+
+let myInformation = {
+  name: "Ethan Ebel",
+  age: 22,
+  randomFact: "a collectible coin dealer",
+  statement () {
+    console.log('Hello my name is ' + myInformation.name + ' will be 32' + ' in 10 years and my hobby is being ' + myInformation.randomFact)
+  },
+  ageincrementor () {
+    myInformation.age++
+  },
+  identityCrisis () {
+    myInformation.name = 'Rumplestillskin'
+    console.log(myInformation)
   }
 }

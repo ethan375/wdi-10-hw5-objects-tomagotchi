84715076f9e967d1d8718bdfa4ddfe293d85efaf
ethan375 <ethanebel1@gmail.com>
2017-12-18 08:32:45 -0600

    Commit 16 - made a 2nd Tamogatchi

--- tamagotchi/tamagotchi.js
+++ tamagotchi/tamagotchi.js
@@ -15,5 +15,29 @@ var tamagotchi1 = {
   // this method makes the thing puke, but doesnt empty the foodInTummy
   puke () {
     console.log('Bleeeeh :O~~')
+    tamagotchi1.foodInTummy -= 5;
+    console.log('there is only ' + this.foodInTummy + ' left in its tummy');
+  }
+}
+
+
+var tamagotchi2 = {
+  name: "billyjoe",
+  creatureType: "wolf",
+  foodInTummy: 9,
+  restedness: 3,
+  health: 7,
+  // this methos makes it seem as though the tamagotchi needs attention
+  cry () {
+    console.log('whaaaaaa!! cried ' + this.name);
+    // this takes food away from the tamagotchis tummy every time it is run. the next line displays that fact
+    tamagotchi1.foodInTummy -= 2;
+    console.log('there is only ' + this.foodInTummy + ' left in its tummy');
+  },
+  // this method makes the thing puke, but doesnt empty the foodInTummy
+  puke () {
+    console.log('Bleeeeh :O~~');
+     tamagotchi1.foodInTummy -= 5;
+    console.log('there is only ' + this.foodInTummy + ' left in its tummy');
   }
 }

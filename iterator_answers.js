bbd95dc7c0a0e3c02424f99ab541f672e5deb5fc
ethan375 <ethanebel1@gmail.com>
2017-12-18 07:48:51 -0600

    Commit 9 - map done

--- iterator_answers.js
+++ iterator_answers.js
@@ -3,3 +3,12 @@ const words = ["Ho", "Chi", "Minh", "City", "was", "once", "known", "as", "Prey"
 words.forEach(function(){
   console.log(words)
   });
+
+
+//Map
+const more_words = ["Joe", "Overeats", "Eggs"];
+//map takes an argument as a function 
+// so somehow using map we iterate through the array and capture just the first letter of every index.
+more_words.map(function() {
+  console.log(more_words)
+})

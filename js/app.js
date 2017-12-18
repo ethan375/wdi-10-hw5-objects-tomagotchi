2f980ab91addda624d6434c45aed45b92b1b374a
ethan375 <ethanebel1@gmail.com>
2017-12-18 06:46:24 -0600

    Commit 7 - Calculator done

--- js/app.js
+++ js/app.js
@@ -47,6 +47,83 @@ let greeter = {
 }
 
 
+// collecting strings 
+let stringCollector = {
+  //collection is an empty array
+  collection: [],
+  //collect is a method that takes a string as a parameter reverses it and puts it into the collect array
+  collect (str) {
+    str.split("").reverse;
+    stringCollector.collection.push(str);
+  },
+  // admirecollection method logs evrything in collection
+  admireCollection () {
+    console.log(collection)
+  }
+}
+
+  // ATM
+  let atm = {
+    //object called at with two properties
+    totalCash: 100000
+    // dispenseTwenties method is going to dispense twenties with an if else statement 
+    dispenseTwenties (w) {
+      if(w<totalCash){
+        totalCash - w
+      } 
+    } return null
+  }
+
+
+
+
+  // hungry turtles
+  let leonardo = {
+    name: 'leonardo',
+    color: 'blue',
+    weapon: 'katana',
+    pizzaEaten: false,
+    // when this method is run it evaluates the current condition of the pizzaEaten property and then changes it accordingly while displaying a message
+    eatPizza () {
+      if(pizzaEaten == false){
+        console.log('leonardo is eating pizza ')
+        leonardo.pizzaEaten = true
+      } else {
+        console.log('Leonardo has already eaten pizza')
+        leonardo.pizzaEaten = false
+      }
+    }
+  }
+
+
+
+// calculator 
+// making an object named calculator with a property output set to 0
+let calculator = {
+  output: 0,
+  // this method multiplies two numbers entered as arguments and sets updates output and displays output
+  multiply (num1, num2) {
+    num1 * num2 = calculator.output;
+    console.log(output);
+  },
+    // this method divides two numbers entered as arguments and sets updates output and displays output
+  divide (num1, num2) {
+    num1 / num2 = calculator.output;
+    console.log(output);
+  },
+    // this method adds two numbers entered as arguments and sets updates output and displays output
+  addition (num1, num2) {
+    num1 + num2 = calculator.output;
+    console.log(calculator.output);
+  },
+    // this method subtractes two numbers entered as arguments and sets updates output and displays output
+  subtraction (num1, num2) {
+    num1 - num2 = calculator.output;
+    console.log(calculator.output);
+  }
+}
+
+
 
 
 
